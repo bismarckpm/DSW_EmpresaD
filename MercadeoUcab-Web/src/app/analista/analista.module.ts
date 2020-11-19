d06be693214d919ff74770339da3c8bf107ffb0a
OscarM04 <oscarmarquez2218@gmail.com>
2020-11-18 22:55:03 -0400

    [Add] Boilerplate del proyecto

---
+++ MercadeoUcab-Web/src/app/analista/analista.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AnalistaRoutingModule } from './analista-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AnalistaRoutingModule
+  ]
+})
+export class AnalistaModule { }
---
+++ MercadeoUcab-Web/src/app/encuestado/encuestado.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EncuestadoRoutingModule } from './encuestado-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EncuestadoRoutingModule
+  ]
+})
+export class EncuestadoModule { }
