dc939b8ab432d4090d069e7c1d38c6edc91aaa51
OscarM04 <oscarmarquez2218@gmail.com>
2020-11-24 22:02:16 -0400

    refactor: :art: Cambios en el boilerplate e importaciones en appmodule
    Se agrego la carpeta Pages a los diferentes modulos

--- MercadeoUcab-Web/src/app/app.module.ts
+++ MercadeoUcab-Web/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminModule } from './admin/admin.module';
+import { AnalistaModule } from './analista/analista.module';
+import { ClienteModule } from './cliente/cliente.module';
+import { EncuestadoModule } from './encuestado/encuestado.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    EncuestadoModule,
+    ClienteModule,
+    AnalistaModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
