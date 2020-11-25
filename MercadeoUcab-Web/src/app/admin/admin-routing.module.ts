3600f2f2b7e7498d5e5489a254253bba025c2f46
OscarM04 <oscarmarquez2218@gmail.com>
2020-11-24 23:19:38 -0400

    refactor: :art: Ruteo inicial, prueba css
    Prueba del ruteo de la pagina, agregadas paginas principales administrador encuestado

--- MercadeoUcab-Web/src/app/admin/admin-routing.module.ts
+++ MercadeoUcab-Web/src/app/admin/admin-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminHomeComponent } from './pages/AdminHome/AdminHome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'administrador', 
+    component:AdminHomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- MercadeoUcab-Web/src/app/encuestado/encuestado-routing.module.ts
+++ MercadeoUcab-Web/src/app/encuestado/encuestado-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/Home/Home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'encuestado', 
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
