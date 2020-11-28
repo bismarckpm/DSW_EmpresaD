4db9e8f934b2c1934b9387e9c44451516892afdb
OscarM04 <oscarmarquez2218@gmail.com>
2020-11-27 23:14:54 -0400

    feat: :sparkles: Pruebas carousel

--- MercadeoUcab-Web/src/app/encuestado/encuestado.module.ts
+++ MercadeoUcab-Web/src/app/encuestado/encuestado.module.ts
@@ -5,11 +5,13 @@ import { EncuestadoRoutingModule } from './encuestado-routing.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { HomeComponent } from './pages/Home/Home.component';
 import { MaterialModule } from '../material.module';
+import { CarrouselComponent } from './components/carrousel/carrousel.component';
 
 
 @NgModule({
   declarations: [
     ToolbarComponent,
+    CarrouselComponent,
     HomeComponent
   ],
   imports: [
