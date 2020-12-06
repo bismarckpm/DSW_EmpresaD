e6c8dd90e8ff56bd7de263d6903784d8485a2c98
OscarM04 <oscarmarquez2218@gmail.com>
2020-12-06 00:50:37 -0400

    feat: :sparkles: Entidad telefono mapeada
    Correcciones adicionales en solicitud test

--- backend/dtos/src/main/java/mercadeoucab/dtos/DtoTelefono.java
+++ backend/dtos/src/main/java/mercadeoucab/dtos/DtoTelefono.java
@@ -1,2 +1,30 @@
-package mercadeoucab.dtos;public class DtoTelefono {
+package mercadeoucab.dtos;
+
+public class DtoTelefono extends DtoBase{
+
+    private String telefono;
+    private DtoDatoEncuestado datoEncuestado;
+
+    public DtoTelefono(long id) throws Exception {
+        super(id);
+    }
+
+    public DtoTelefono() {
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public DtoDatoEncuestado getDatoEncuestado() {
+        return datoEncuestado;
+    }
+
+    public void setDatoEncuestado(DtoDatoEncuestado datoEncuestado) {
+        this.datoEncuestado = datoEncuestado;
+    }
 }
