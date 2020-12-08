466a599d2017a3d11bcf6807166ead33b6dd0151
Neradkiller <daren1997gonzalez@gmail.com>
2020-12-07 20:27:34 -0400

    feat: :sparkles: Mapeo de Hijo(entidad, DTO, dao)

---
+++ backend/dtos/src/main/java/mercadeoucab/dtos/DtoHijo.java
@@ -0,0 +1,42 @@
+package mercadeoucab.dtos;
+
+import java.sql.Date;
+
+public class DtoHijo extends DtoBase{
+
+
+    private String genero;
+    private Date edad;
+    private DtoDatoEncuestado fk_dato_encuestado;
+
+    public DtoHijo(long id) throws Exception {
+        super(id);
+    }
+
+    public DtoHijo() {
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Date getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Date edad) {
+        this.edad = edad;
+    }
+
+    public DtoDatoEncuestado getFk_dato_encuestado() {
+        return fk_dato_encuestado;
+    }
+
+    public void setFk_dato_encuestado(DtoDatoEncuestado fk_dato_encuestado) {
+        this.fk_dato_encuestado = fk_dato_encuestado;
+    }
+}
