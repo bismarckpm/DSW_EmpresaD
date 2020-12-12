59203395968e2ef71fbeb258fb366658a2bfa0b7
Neradkiller <daren1997gonzalez@gmail.com>
2020-12-12 11:28:22 -0400

    refactor: :art: Subcategoria usa responses

--- backend/servicio/src/test/java/ServicioSubCategoriaTest.java
+++ backend/servicio/src/test/java/ServicioSubCategoriaTest.java
@@ -6,6 +6,7 @@ import mercadeoucab.servicio.ServicioSubCategoria;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 public class ServicioSubCategoriaTest {
@@ -15,66 +16,51 @@ public class ServicioSubCategoriaTest {
         ServicioSubCategoria servicio = new ServicioSubCategoria();
         DtoSubCategoria dtoSubCategoria = new DtoSubCategoria();
         dtoSubCategoria.setNombre( "Bebida");
-        // Se debe tener una Categoria en la BD
-        dtoSubCategoria.setCategoria( new DtoCategoria(2));
-        DtoSubCategoria resultado = servicio.registrarSubCategoria( dtoSubCategoria);
-        Assert.assertNotEquals( resultado.get_id(), 0);
+        dtoSubCategoria.setCategoria( new DtoCategoria(1));
+        Response resultado = servicio.registrarSubCategoria( dtoSubCategoria);
+        Assert.assertEquals(resultado.getStatus(), 200);
     }
 
     @Test
     public void actualizarSubCategoriaTest() throws Exception {
         ServicioSubCategoria servicio = new ServicioSubCategoria();
-        DtoSubCategoria dtoSubCategoria = new DtoSubCategoria();
-        dtoSubCategoria.setNombre( "Soy una subcategoria");
-        // Se debe tener una Categoria en la BD
-        dtoSubCategoria.setCategoria( new DtoCategoria(2));
-        DtoSubCategoria paraActualizar = servicio.registrarSubCategoria(
+        DtoSubCategoria dtoSubCategoria = new DtoSubCategoria(1);
+        dtoSubCategoria.setCategoria( new DtoCategoria(1));
+        dtoSubCategoria.setNombre("Soy una subc actualizada");
+        Response resultado = servicio.actualizarSubCategoria(
+                dtoSubCategoria.get_id(),
                 dtoSubCategoria
         );
-        paraActualizar.setNombre("Soy una subc actualizada");
-        DtoSubCategoria resultado = servicio.actualizarSubCategoria(
-                paraActualizar.get_id(),
-                paraActualizar
-        );
-        Assert.assertNotEquals(resultado.get_id(), 0);
+        Assert.assertEquals(resultado.getStatus(), 200);
     }
 
     @Test
     public void eliminarSubCategoriaTest() throws Exception {
         ServicioSubCategoria servicio = new ServicioSubCategoria();
-        DtoSubCategoria dtoSubCategoria = new DtoSubCategoria();
+        DtoSubCategoria dtoSubCategoria = new DtoSubCategoria(1);
         dtoSubCategoria.setNombre( "Soy una subcategoria borrada");
-        // Se debe tener una Categoria en la BD
-        dtoSubCategoria.setCategoria( new DtoCategoria(2));
-        DtoSubCategoria paraBorrar = servicio.registrarSubCategoria(
-                dtoSubCategoria
+        dtoSubCategoria.setCategoria( new DtoCategoria(1));
+        Response resultado = servicio.eliminarSubCategoria(
+                dtoSubCategoria.get_id()
         );
-        DtoSubCategoria resultado = servicio.eliminarSubCategoria(
-                paraBorrar.get_id()
-        );
-        Assert.assertEquals(resultado.get_id(), paraBorrar.get_id());
+        Assert.assertEquals(resultado.getStatus(), 200);
     }
 
     @Test
     public void obtenerSubCategoriaTest() throws Exception {
         ServicioSubCategoria servicio = new ServicioSubCategoria();
-        DtoSubCategoria dtoSubCategoria = new DtoSubCategoria();
-        dtoSubCategoria.setNombre( "Bebida");
-        // Se debe tener una Categoria en la BD
-        dtoSubCategoria.setCategoria( new DtoCategoria(2));
-        DtoSubCategoria paraConsultar = servicio.registrarSubCategoria(
-                dtoSubCategoria
+        DtoSubCategoria dtoSubCategoria = new DtoSubCategoria(1);
+        dtoSubCategoria.setCategoria( new DtoCategoria(1));
+        Response resultado = servicio.obtenerSubCategoria(
+                dtoSubCategoria.get_id()
         );
-        DtoSubCategoria resultado = servicio.obtenerSubCategoria(
-                paraConsultar.get_id()
-        );
-        Assert.assertEquals( resultado.get_id(), paraConsultar.get_id());
+        Assert.assertEquals(resultado.getStatus(), 200);
     }
 
     @Test
     public void listarSubCategoriaTest() throws Exception {
         ServicioSubCategoria servicio = new ServicioSubCategoria();
-        List<SubCategoria> resultado = servicio.listarSubCategoria();
-        Assert.assertNotNull( resultado);
+        Response resultado = servicio.listarSubCategoria();
+        Assert.assertEquals(resultado.getStatus(), 200);
     }
 }
