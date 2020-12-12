9768a0ce1410922a3f6ad8c1cb11ff104fe04e80
Neradkiller <daren1997gonzalez@gmail.com>
2020-12-12 00:55:44 -0400

    refactor: :zap: Añadido el manejo de Response a las respuestas de categoria

--- backend/servicio/src/test/java/ServicioCategoriaTest.java
+++ backend/servicio/src/test/java/ServicioCategoriaTest.java
@@ -17,42 +17,35 @@ public class ServicioCategoriaTest {
         Response resultado = servicio.agregarCategoria(categoria);
         Assert.assertEquals(200, resultado.getStatus());
     }
-    /*
+
     @Test
     public void consultarCategoriaTest() throws  Exception{
         ServicioCategoria servicio = new ServicioCategoria();
-        DtoCategoria categoria = new DtoCategoria();
-        categoria.setNombre("Higiene");
-        Response consultar = servicio.agregarCategoria(categoria);
-        Categoria consultado = servicio.consultarCategoria(consultar.get_id());
-        Assert.assertEquals(consultar.get_id(), consultado.get_id());
+        Response resultado = servicio.consultarCategoria(1);
+        Assert.assertEquals(200, resultado.getStatus());
     }
+
 
     @Test
     public void actualizarCategoriaTest() throws Exception{
         ServicioCategoria servicio = new ServicioCategoria();
         DtoCategoria categoria = new DtoCategoria();
-        categoria.setNombre("Higiene");
-        Categoria actualizar = servicio.agregarCategoria(categoria);
         categoria.setNombre("Nuevo");
-        Categoria actualizado = servicio.actualizarCategoria(actualizar.get_id(), categoria);
-        Assert.assertNotNull(actualizado.getModificado_el());
+        Response resultado = servicio.actualizarCategoria(1, categoria);
+        Assert.assertEquals(200, resultado.getStatus());
     }
 
     @Test
     public  void eliminarCategoriaTest() throws Exception{
         ServicioCategoria servicio = new ServicioCategoria();
-        DtoCategoria categoria = new DtoCategoria();
-        categoria.setNombre("Higiene");
-        Categoria eliminar = servicio.agregarCategoria(categoria);
-        Categoria eliminado = servicio.eliminarCategoria(eliminar.get_id());
-        Assert.assertEquals(0, eliminado.getActivo());
+        Response eliminado = servicio.eliminarCategoria(1);
+        Assert.assertEquals(200, eliminado.getStatus());
     }
 
     @Test
     public void listarCategoriasTest() throws  Exception{
         ServicioCategoria servicio = new ServicioCategoria();
-        List<Categoria> categorias = servicio.listarCategorias();
-        Assert.assertNotNull(categorias);
-    }*/
+        Response categorias = servicio.listarCategorias();
+        Assert.assertEquals(200, categorias.getStatus());
+    }
 }
