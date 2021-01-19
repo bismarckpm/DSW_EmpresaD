468ff1c7d188087c3d3c59d25fa505139e6bfcf3
DCodex <sampleacc620@gmail.com>
2021-01-19 02:29:19 -0400

    *Refactor estructural del dashboard de administrador implementado

--- MercadeoUcab-Web/src/app/admin/pages/dashboard/dashboard.component.ts
+++ MercadeoUcab-Web/src/app/admin/pages/dashboard/dashboard.component.ts
@@ -8,8 +8,19 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class DashboardComponent implements OnInit {
   constructor(private route: ActivatedRoute, private router: Router) {}
   //CONTROL DE MUESTRA DE NOTIFICACIONES
+  sections: any[] = [
+  { title:'Usuarios',dir:'usuarios'},
+  { title:'Estudios',dir:'estudios'},
+  { title:'Home',dir:'home'},
+  { title:'Locaciones',dir:'lugares'},
+  { title:'Preguntas',dir:'preguntas'},
+  { title:'Categorias',dir:'categorias'},
+  { title:'SubCategorias',dir:'subcategorias'},
+  { title:'Tipos',dir:'tipos'},
+  { title:'Presentaciones',dir:'presentaciones'},
+  ];
   openNote: boolean=false;
-  
+  showFiller = false;
   getOpenNote():boolean{
     return this.openNote;
   };
@@ -25,7 +36,7 @@ export class DashboardComponent implements OnInit {
   onDir(_route: string): void {
     try {
       //console.log(_route);
-      this.router.navigate([_route]);
+      this.router.navigate(['administrador/',_route]);
     } catch (e) {
       console.log(e.message);
     }
