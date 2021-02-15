395f331db323aeaf4c58b404c99f755a4459fa2f
Neradkiller <daren1997gonzalez@gmail.com>
2021-02-15 07:40:27 -0400

    fix: :bug: fixed cambio de estado

--- backend/logica/src/main/java/mercadeoucab/comandos/Estudio/ComandoAgregarEstudio.java
+++ backend/logica/src/main/java/mercadeoucab/comandos/Estudio/ComandoAgregarEstudio.java
@@ -1,10 +1,12 @@
 package mercadeoucab.comandos.Estudio;
 
 import mercadeoucab.accesodatos.DaoEstudio;
+import mercadeoucab.accesodatos.DaoSolicitud;
 import mercadeoucab.comandos.ComandoAbstracto;
 import mercadeoucab.comandos.ComandoBase;
 import mercadeoucab.dtos.DtoEstudio;
 import mercadeoucab.entidades.Estudio;
+import mercadeoucab.entidades.Solicitud;
 import mercadeoucab.fabricas.Enums.Fabricas;
 import mercadeoucab.fabricas.FabricaAbstracta;
 import mercadeoucab.mappers.EstudioMapper;
@@ -26,11 +28,19 @@ public class ComandoAgregarEstudio extends ComandoAbstracto implements ComandoBa
         try
         {
             FabricaAbstracta fabrica = FabricaAbstracta.getFactory(Fabricas.ESTUDIO);
+            FabricaAbstracta fabricaSolicitud = FabricaAbstracta.getFactory(Fabricas.SOLICITUD);
             DaoEstudio dao = (DaoEstudio) fabrica.generarDao();
             Estudio estudio = EstudioMapper.mapDtotoEntity(dtoEstudio);
             estudio.setActivo(1);
             estudio.setCreado_el(new Date(Calendar.getInstance().getTime().getTime()));
             Estudio resul = dao.insert(estudio);
+
+            DaoSolicitud daoSolicitud = (DaoSolicitud) fabricaSolicitud.generarDao();
+            Solicitud solicitud =  daoSolicitud.find(dtoEstudio.getSolicitud().get_id(), Solicitud.class);
+            solicitud.setEstado("aceptada");
+            solicitud.setModificado_el(new Date(Calendar.getInstance().getTime().getTime()));
+            daoSolicitud.update(solicitud);
+
             result = ResponseGeneral.SuccesCreate( resul.get_id());
         }
         catch (Exception e){
